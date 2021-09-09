e12888b322ea4e41cc1b4b27679648274ac62630
Roman Sterlin <Feuse132@gmail.com>
2021-09-09 16:00:52 +0300

    updated

---
+++ ServicesDataAccess.cs
@@ -0,0 +1,164 @@
+﻿using BadooAPI.Utills;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using RabbitMQScheduler.Models;
+using ServicesInterfaces;
+using ServicesModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ServicesDataAccess : IDataAccess
+    {
+        private readonly IMongoCollection<UserCredentials> _userCredentials;
+        private readonly IMongoCollection<ServiceSessions> _serviceSessions;
+        public ServicesDataAccess(IAutoLoverDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _userCredentials = database.GetCollection<UserCredentials>(settings.UserCredentialsCollectionName);
+            _serviceSessions = database.GetCollection<ServiceSessions>(settings.ServiceSessionsCollectionName);
+        }
+        public async Task<UpdateResult> RegisterService(Data data)
+        {
+            var filter = Builders<UserCredentials>.Filter.Eq("_id", new ObjectId(data.Id));
+            ///hash password before insert
+            UserServiceCredentials userServiceCredentials = new UserServiceCredentials()
+            {
+                UserServiceId = data.UserServiceId,
+                Username = data.UserName,
+                Password = data.Password,
+                Service = data.Service,
+                Hash = ""
+            };
+
+            var result = await _userCredentials.UpdateOneAsync(filter,
+                  Builders<UserCredentials>.Update.AddToSet(u => u.Services, userServiceCredentials));
+            await UpdateServiceSession(data);
+            
+            return result;
+        }
+
+        public async Task<ServiceSessions> CheckForServiceSession(Data data)
+        {
+            try
+            {
+                var filter = Builders<ServiceSessions>.Filter.Eq("SerivceId", data.UserServiceId);
+
+                return await (await _serviceSessions.FindAsync(filter)).FirstAsync();
+            }
+            catch (Exception)
+            {
+
+                return default(ServiceSessions);
+            }
+        }
+
+        public async Task UpdateServiceSession(Data data)
+        {
+            var today = DateTime.Today;
+            ServiceSessions session = new ServiceSessions()
+            {
+                Id = data.Id,
+                SerivceId = data.UserServiceId,
+                SessionId = data.SessionId,
+                HiddenUrl = data.HiddenUrl,
+                XPing = data.XPing,
+                ExpireAt = today.AddDays(1)
+            };
+
+            await _serviceSessions.InsertOneAsync(session);
+
+        }
+
+        public async Task<UserServiceCredentials> GetUserServiceByServiceNameAndId(Data data)
+        {
+            var filter = Builders<UserCredentials>.Filter.Eq("_id", new ObjectId(data.Id));
+            
+            var collection = await _userCredentials.Find(filter).SingleOrDefaultAsync();
+
+            if (collection is not null)
+            {
+                var service = collection.Services.Where(a => a.Service == data.Service).First();
+                return service;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<UserServiceCredentials>> GetAllUserServicesById(string id)
+        {
+            var filter = Builders<UserCredentials>.Filter.Eq("_id", new ObjectId(id));
+           // var userServiceCredentialsCollection = _database.GetCollection<UserCredentials>("UserCredentials");
+            var collection = await _userCredentials.Find(filter).SingleOrDefaultAsync();
+
+            if (collection is not null)
+            {
+                var services = collection.Services.ToList();
+                return services;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<DeleteResult> RemoveServiceFromUser(Data data)
+        {
+           // var userCredentialsCollection = _database.GetCollection<UserCredentials>("UserCredentials");
+            var filter = Builders<UserCredentials>.Filter.Where(ym => ym.Id == data.Id);
+            var update = Builders<UserCredentials>.Update.PullFilter(ym => ym.Services, Builders<UserServiceCredentials>.Filter.Where(nm => nm.Service == data.Service));
+            await _userCredentials.UpdateOneAsync(filter, update);
+
+            //var userServiceCredentialsCollection = _database.GetCollection<ServiceSessions>("test");
+            var filterService = Builders<ServiceSessions>.Filter.Where(ym => ym.Id == data.Id && ym.SessionId == data.SessionId);
+
+            return await _serviceSessions.DeleteOneAsync(filterService);
+
+        }
+
+        public async Task<UserCredentials> AuthenticateUser(Data data)
+        {
+          //  var userServiceCredentialsCollection = _database.GetCollection<UserCredentials>("UserCredentials");
+            var filter = Builders<UserCredentials>.Filter.Where(ym => ym.Username == data.UserName & ym.Password == data.Password);
+
+            var user = await _userCredentials.Find(filter).SingleOrDefaultAsync();
+            return user;
+
+        }
+        public async Task<UserCredentials> CheckIfUsernameExists(Data data)
+        {
+
+        //    var userServiceCredentialsCollection = _database.GetCollection<UserCredentials>("UserCredentials");
+            var filter = Builders<UserCredentials>.Filter.Where(ym => ym.Username == data.UserName);
+
+            var user = await _userCredentials.Find(filter).SingleOrDefaultAsync();
+            return user;
+
+        }
+        public async Task<UserCredentials> RegisterUser(Data data)
+        {
+           // var collection = _database.GetCollection<UserCredentials>("UserCredentials");
+
+            UserCredentials user = new UserCredentials()
+            {
+                Username = data.UserName,
+                Password = data.Password,
+                Services = new List<UserServiceCredentials>()
+            };
+
+            await _userCredentials.InsertOneAsync(user);
+            return user;
+
+        }
+    }
+}
---
+++ ServicesDataAccessCache.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using MongoDB.Driver;
+using ServicesInterfaces;
+using ServicesModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ServicesDataAccessCache : IDataAccess 
+    {
+
+        private readonly IDistributedCache _distributedCache;
+        public ServicesDataAccessCache(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+            
+        }
+
+        public async Task<ServiceSessions> CheckForServiceSession(Data data)
+        {
+            var result = await _distributedCache.GetStringAsync(data.UserServiceId);
+            
+            if (result is null)
+            {
+                return default;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserCredentials> CheckIfUsernameExists(Data data)
+        {
+            var result = await _distributedCache.GetStringAsync(data.UserName);
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<UserServiceCredentials>> GetAllUserServicesById(Data data)
+        {
+            var result = await _distributedCache.GetStringAsync(data.UserServiceId);
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserServiceCredentials> GetUserServiceByServiceNameAndId(Data data)
+        {
+            var result = await _distributedCache.GetStringAsync(data.Service.ToString());
+            throw new NotImplementedException();
+        }
+        public Task<UserCredentials> AuthenticateUser(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateResult> RegisterService(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserCredentials> RegisterUser(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteResult> RemoveServiceFromUser(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateServiceSession(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserServiceCredentials>> GetAllUserServicesById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
